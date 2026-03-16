a20f58cd4e28d3283195a21f40b4d64766e2e812
Tobbe7612 <123266550+Tobbe7612@users.noreply.github.com>
2026-03-16 23:38:41 +0100

    Add FlowData interface and readFlows function

--- src/flow.ts
+++ src/flow.ts
@@ -1,1 +1,34 @@
+export interface FlowData {
+solarHouse:number
+solarBattery:number
+solarExport:number
+batteryHouse:number
+batteryGrid:number
+gridHouse:number
+gridBattery:number
+houseGrid:number
+}
 
+export function readFlows(hass:any):FlowData{
+
+const get=(e:string)=>Number(hass.states[e]?.state || 0)
+
+return {
+
+```
+solarHouse:get("sensor.power_solar_house"),
+solarBattery:get("sensor.power_solar_battery"),
+solarExport:get("sensor.power_solar_export"),
+
+batteryHouse:get("sensor.power_battery_house"),
+batteryGrid:get("sensor.power_battery_grid"),
+
+gridHouse:get("sensor.power_grid_house"),
+gridBattery:get("sensor.power_grid_battery"),
+
+houseGrid:get("sensor.power_house_grid")
+```
+
+}
+
+}
